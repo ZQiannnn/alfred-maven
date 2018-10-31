13a7bfbac41315bb13a52a9270410027a7db9a40
ZQian <qian.zhang@keyrus.com>
2018-10-31 09:14:50 +0800

    Maven Search

--- index.js
+++ index.js
@@ -1,9 +1,107 @@
 'use strict';
 const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
+// 搜索模式
+// <artifactId>
+// <artifactId> <version>
+// <groupId> <artifactId> <version>
+// 任意一个都可以被*代替
+let query = {groupId: '', artifactId: '', version: '', repoId: 'central'};
+let field3 = ['groupId', 'artifactId', 'version'];
+let field2 = ['artifactId', 'version'];
+let field1 = ['artifactId'];
+let keywords = alfy.input.split(' ');
+if (keywords.length === 3) {
+	keywords.forEach((v, i) => {
+		if (v !== '*') {
+			query[field3[i]] = v;
+		}
+	});
+}
+if (keywords.length === 2) {
+	keywords.forEach((v, i) => {
+		if (v !== '*') {
+			query[field2[i]] = v;
+		}
+	});
+}
+if (keywords.length === 3) {
+	keywords.forEach((v, i) => {
+		if (v !== '*') {
+			query[field3[i]] = v;
+		}
+	});
+}
+if (keywords.length === 1) {
+	keywords.forEach((v, i) => {
+		if (v !== '*') {
+			query[field1[i]] = v;
+		}
+	});
+}
+alfy.fetch('http://maven.aliyun.com/artifact/aliyunMaven/searchArtifactByGav', {
+	query: query
+}).then(data => {
+	let result = data.object.filter(m => {
+		// 过滤非jar
+		return m.packaging === 'jar';
+	}).slice(0, 20).map(m => {
+		// 整理成alfred
+		const mvn = `<dependency>\n  <groupId>${m.groupId}</groupId>\n  <artifactId>${m.artifactId}</artifactId>\n  <version>${m.version}</version>\n</dependency>`;
+		const gradle = `compile '${m.groupId}:${m.artifactId}:${m.version}'`;
+		const url = 'https://mvnrepository.com/artifact/${m.groupId}/${m.artifactId}/${m.version}';
+
+		return {
+			title: m.artifactId,
+			subtitle: m.groupId + ':' + m.artifactId + ':' + m.version,
+			autocomplete: m.artifactId,
+			quicklookurl: url,
+			arg: mvn,
+			mods: {
+				cmd: {
+					arg: mvn,
+					subtitle: `Copied as maven format.`
+				},
+				alt: {
+					arg: gradle,
+					subtitle: `Copied as gradle format.`
+				}
+			}
+
+		};
+	});
+	alfy.output(result);
+});
+
+
+alfy.fetch('http://search.maven.org/solrsearch/select', {
+	query: {
+		q,
+		start: 0,
+		rows: 20
 	}
-]);
+}).then(data => {
+	const items = data.response.docs
+		.map(x => {
+			const v = x.v?x.v:x.latestVersion;
+			const mvn = `<dependency>\n  <groupId>${x.g}</groupId>\n  <artifactId>${x.a}</artifactId>\n  <version>${v}</version>\n</dependency>`;
+			const gradle = `compile '${x.g}:${x.a}:${v}'`;
+			return {
+				title: `${x.g}:${x.a}:${v}`,
+				subtitle: `updated at ${dateFormat('yyyy-dd-MM', new Date(x.timestamp))}`,
+				arg: mvn,
+				mods: {
+					cmd: {
+						arg: mvn,
+						subtitle: `copy maven dependency to clipboard`
+					},
+					alt: {
+						arg: gradle,
+						subtitle: `copy gradle dependency to clipboard`
+					}
+				}
+			};
+		});
+
+	alfy.output(items);
+});
--- test.js
+++ test.js
@@ -1,14 +1,8 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
 
-test(async t => {
+test(async () => {
 	const alfy = alfyTest();
-	const result = await alfy('Rainbow');
-
-	t.deepEqual(result, [
-		{
-			title: 'Unicorn',
-			subtitle: 'Rainbow'
-		}
-	]);
+	const result = await alfy('fastjson');
+	console.log(result);
 });
