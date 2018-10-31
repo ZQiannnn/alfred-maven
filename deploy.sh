642c49bac9b884df211960bf95027acc4f23946d
ZQian <qian.zhang@keyrus.com>
2018-10-31 10:40:41 +0800

    Maven Search

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 git add .
 git commit -m "Maven Search"
 npm version patch
-git add .
-git commit -m "Maven Search"
-npm version patch
+npm publish
 npm uninstall -g alfred-maven
 npm install -g alfred-maven
